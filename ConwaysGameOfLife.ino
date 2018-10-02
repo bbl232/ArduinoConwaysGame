e9c0010361875814e11d7a3d01197b16da59c8e1
Bill Vandenberk <bill@vandenberk.me>
2018-10-01 22:24:20 -0400

    Updated to generate next gen separate from current one
    This fixes the weird snakey generation

--- ConwaysGameOfLife.ino
+++ ConwaysGameOfLife.ino
@@ -8,27 +8,26 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define pixel_width 2
 #define pixel_height 2
 #define width 64
-#define height 27
+#define height 26
 
 #define buffer_width 8
 
 #define alive 1
 #define dead 0
-
+      
 #define getCell(cells, x, y) (1 == ((cells[x/8][y] >> (x % 8)) & 1))
 #define setCell(cells, x, y, state) (cells[x/8][y] = (state == 1 ? cells[x/8][y] | (1 << (x % 8)) : cells[x/8][y] & ~(1 << (x % 8))))
 
 uint8_t cells[buffer_width][height];
-unsigned long int generation = 0;
-unsigned long int starttime;
+uint8_t cellsNext[buffer_width][height];
+
+unsigned int generation = 0;
+unsigned int starttime;
 
 void setup() {
   // Seed random here with the floating value from pin0
   pinMode(0, INPUT);
   randomSeed(analogRead(0));
-  
-  Serial.begin(9600); // For debugging
-
   // Display setup stuff
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.display();
@@ -44,7 +43,7 @@ void setup() {
   display.println("3: L > 3 = Death");
   display.println("4: D == 3 = Birth");
   display.display();
-  delay(10000);
+  //delay(10000);
   display.clearDisplay();
   display.display();
   
@@ -69,11 +68,11 @@ int countNeighbours(int x, int y){
   int neighbours = 0;
   for(int i = -1; i < 2; i++){
     for(int j = -1; j < 2; j++){
-      if ((x + i) < 0 || (x + i) > width || (y + j) < 0 || (y + j) > height || (i == 0 && j == 0)){
+      if ((x + i) < 0 || (x + i) >= width || (y + j) < 0 || (y + j) >= height || (i == 0 && j == 0)){
         continue;
       }
 
-      if (cells[(x + i)/8][y + j] & (1 << ((x + i) % 8))){
+      if (getCell(cells, (x + i), (y + j)) == alive){
         neighbours++;
         if (neighbours == 4){
           return 4;
@@ -97,20 +96,30 @@ void loop() {
     display.print((float)generation/secondsElapsed);
   }
   // Display the current generation
-  display.display(); 
   
   // While we calculate the next
   for(int x = 0; x < width; x++){
     for (int y = 0; y < height; y++){
-      int neighbours = countNeighbours(x, y);;
-      if(getCell(cells, x, y) && (neighbours < 2 || neighbours > 3)){
-          setCell(cells, x, y, dead);
-          drawCell(x,y,BLACK);
+      drawCell(x, y, getCell(cells, x, y) ? WHITE : BLACK);
+
+      int neighbours = countNeighbours(x, y);
+      int state = getCell(cells, x, y);
+      
+      if(state == alive && (neighbours < 2 || neighbours > 3)){
+          setCell(cellsNext, x, y, dead);
       }
-      else if(!getCell(cells, x, y) && (neighbours == 3)){
-        setCell(cells, x, y, alive);
-        drawCell(x,y,WHITE);
+      else if(state == dead && (neighbours == 3)){
+        setCell(cellsNext, x, y, alive);
       }
+      else{
+        setCell(cellsNext, x, y, state);
+      }
+    }
+  }
+  display.display(); 
+  for(int x = 0; x < width; x++){
+    for (int y = 0; y < height; y++){
+        setCell(cells, x, y, getCell(cellsNext, x, y));
     }
   }
 }
